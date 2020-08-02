68609261d0e847cb601982d25ce4364d6f885f01
beata <beata@emc2wave.com>
2020-08-02 15:52:34 -0700

    add containerHeight linksHeight use getBounding..

--- app.js
+++ app.js
@@ -14,8 +14,16 @@ const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', () => {
   // linksContainer.classList.toggle('show-links');
-  const containerHeight = linksContainer.getBoundingClientRect();
-  console.log(containerHeight);
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  // console.log(containerHeight);
+  const linksHeight = links.getBoundingClientRect().height;
+  // console.log(linksHeight);
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
 });
 
 // ********** fixed navbar ************
