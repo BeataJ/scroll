03c8392c8202291db3f0011beab89c1c510301b1
beata <beata@emc2wave.com>
2020-08-03 14:14:14 -0700

    add position, window.scrollTo, hide linksContainer

--- app.js
+++ app.js
@@ -56,5 +56,11 @@ scrollLinks.forEach((link) => {
     // navigate to specific spot
     const id = e.currentTarget.getAttribute('href').slice(1);
     const element = document.getElementById(id);
+    let position = element.offsetTop;
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
   });
 });
