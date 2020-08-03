9b2e4a5051999875f9bbc261796f2a4e35fe3931
beata <beata@emc2wave.com>
2020-08-02 17:32:06 -0700

    add scrollHeight > 500

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ window.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('fixed-nav');
   }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
 });
 
 // ********** smooth scroll ************
